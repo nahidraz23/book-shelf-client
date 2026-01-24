c99b28f15a8c0d09f2471cdb938a972a730bb17a
nahidraz23 <nzamanraz@gmail.com>
2026-01-24 21:59:26 +0600

    chore: main layout implemented using react-router

---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router";
+
+const MainLayout = () => {
+    return (
+        <div>
+            <div>
+                
+            </div>
+            <Outlet/>
+        </div>
+    );
+};
+
+export default MainLayout;
