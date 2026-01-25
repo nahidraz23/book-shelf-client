4257fc343716c1e3771f538f1f95dd0edc882fad
nahidraz23 <nzamanraz@gmail.com>
2026-01-25 12:44:28 +0600

    feat: store setup and RTK query setup for

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { router } from './routes/router.ts'
-import { RouterProvider } from 'react-router'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { router } from "./routes/router.ts";
+import { RouterProvider } from "react-router";
+import { Provider } from "react-redux";
+import { store } from "./redux/store.ts";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router}/>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </StrictMode>,
-)
+);
---
+++ src/pages/AllBooks.tsx
@@ -0,0 +1,9 @@
+const AllBooks = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default AllBooks;
