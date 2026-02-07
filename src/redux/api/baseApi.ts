177fc25ed87e59aa912cd565bd873379cec6c2d7
nahidraz23 <nzamanraz@gmail.com>
2026-02-07 16:39:15 +0600

    chore: baseApi updated with production endpoint

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -2,7 +2,7 @@ import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 
 export const baseApi = createApi({
     reducerPath: 'baseApi',
-    baseQuery: fetchBaseQuery({baseUrl: 'http://localhost:5001/api'}),
+    baseQuery: fetchBaseQuery({baseUrl: 'https://library-service-api.vercel.app/api'}),
     tagTypes: ['books'],
     endpoints: (builder) => ({
         getBooks: builder.query({
